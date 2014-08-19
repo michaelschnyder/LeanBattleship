9816819ea0f4fbcc7137bd55a69c36a728e6c1c8
Michael Schnyder <michael.schnyder@outlook.com>
2014-08-20 01:52:07 +0200

    added some more stuff

---
+++ Source/LeanBattleship.Web/Controllers/GameEventController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Concurrent;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace LeanBattleship.Web.Controllers
+{
+    public class GameEventController : ApiController
+    {
+        private static readonly ConcurrentDictionary<string, ConcurrentQueue<StreamWriter>> AllClients = new ConcurrentDictionary<string, ConcurrentQueue<StreamWriter>>();
+
+
+        [HttpGet]
+        [Route("api/game/{gameId}/events")]
+        public HttpResponseMessage GetEventStreamForGame(string gameId)
+        {
+            var response = this.Request.CreateResponse();
+            response.Content = new PushStreamContent((stream, httpContent, transportContext) => this.StreamIsAvailable(gameId, stream, httpContent, transportContext), "text/event-stream");
+            return response;
+        }
+
+        private void StreamIsAvailable(string gameId, Stream stream, HttpContent httpContent, TransportContext transportContext)
+        {
+            var streamwriter = new StreamWriter(stream);
+
+            AllClients.AddOrUpdate(gameId, key => new ConcurrentQueue<StreamWriter>(new[] {streamwriter}),
+                (s, queue) =>
+                {
+                    queue.Enqueue(streamwriter);
+                    return queue;
+                });
+        }
+
+        public static void SendEvent(string gameId, GameEvent m)
+        {
+            ConcurrentQueue<StreamWriter> registeredClients;
+
+            if (AllClients.TryGetValue(gameId, out registeredClients))
+            {
+                foreach (var subscriber in registeredClients)
+                {
+                    subscriber.WriteLine("data:" + JsonConvert.SerializeObject(m) + "\n");
+                    subscriber.Flush();
+                }
+            }
+
+        }
+    }
+
+    public abstract class GameEvent
+    {
+    }
+}
--- Source/LeanBattleship.Web/Controllers/PlayController.cs
+++ Source/LeanBattleship.Web/Controllers/PlayController.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.Practices.ServiceLocation;
 
 namespace LeanBattleship.Web.Controllers
 {
@@ -13,13 +13,31 @@ namespace LeanBattleship.Web.Controllers
         [Route("api/game/{gameId}/state")]
         public IHttpActionResult GetStateForGame(string gameId)
         {
+            var tournament = ServiceLocator.Current.GetInstance<ITournamentService>();
+
+            var game = tournament.GetGame(gameId);
+
+            if (game == null)
+            {
+                return this.NotFound();
+            }
+
             return this.Ok();
         }
 
-        [HttpPost]
-        [Route("api/game/{gameId}/setup")]
-        public IHttpActionResult SetupShipsForGame(string gameId)
+        [HttpPut]
+        [Route("api/game/{gameId}/ships")]
+        public IHttpActionResult SetupShipsForGame(string gameId, List<ShipPlacement> )
         {
+            var tournament = ServiceLocator.Current.GetInstance<ITournamentService>();
+
+            var game = tournament.GetGame(gameId);
+
+            if (game == null)
+            {
+                return this.NotFound();
+            }
+
             return this.Ok();
         }
 
@@ -27,7 +45,39 @@ namespace LeanBattleship.Web.Controllers
         [Route("api/game/{gameId}/fire/{position}")]
         public IHttpActionResult FireInGame(string gameId, string position)
         {
+            var tournament = ServiceLocator.Current.GetInstance<ITournamentService>();
+
+            var game = tournament.GetGame(gameId);
+
+            if (game == null)
+            {
+                return this.NotFound();
+            }
+
+            if (game.Fire())
+            {
+                return this.Ok();
+            }
+
             return this.Ok();
+        }
+    }
+
+    public class ShipPlacement
+    {
+        private List<KeyValuePair<string, string>> positionsList = new List<KeyValuePair<string, string>>();
+    }
+
+    public interface ITournamentService
+    {
+        BattleShipGame GetGame(string gameId);
+    }
+
+    public class BattleShipGame
+    {
+        public bool Fire()
+        {
+            return true;
         }
     }
 }
