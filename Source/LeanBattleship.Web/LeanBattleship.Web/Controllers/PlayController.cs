6754bd25ac81b19517254e494b0fa672048a7b05
Michael Schnyder <michael.schnyder@outlook.com>
2014-08-20 01:03:45 +0200

    first controllers

---
+++ Source/LeanBattleship.Web/LeanBattleship.Web/Controllers/PlayController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LeanBattleship.Web.Controllers
+{
+    public class PlayController : ApiController
+    {
+        [HttpGet]
+        [Route("api/game/{gameId}/state")]
+        public IHttpActionResult GetStateForGame(string gameId)
+        {
+            return this.Ok();
+        }
+
+        [HttpPost]
+        [Route("api/game/{gameId}/setup")]
+        public IHttpActionResult SetupShipsForGame(string gameId)
+        {
+            return this.Ok();
+        }
+
+        [HttpPost]
+        [Route("api/game/{gameId}/fire/{position}")]
+        public IHttpActionResult FireInGame(string gameId, string position)
+        {
+            return this.Ok();
+        }
+    }
+}
---
+++ Source/LeanBattleship.Web/LeanBattleship.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LeanBattleship.Web.WebApiApplication" Language="C#" %>
