aeeb43703315a5415460f2a22ab05499e7f4467c
Michael Schnyder <michael.schnyder@outlook.com>
2014-08-22 00:19:02 +0200

    moved filr

--- Source/LeanBattleship.Web/Controllers/DefaultController.cs
+++ Source/LeanBattleship.Web/Controllers/DefaultController.cs
@@ -29,11 +29,4 @@ namespace LeanBattleship.Web.Controllers
             });
         }
     }
-
-    public class VersionDto
-    {
-        public string Version { get; set; }
-        public string Build { get; set; }
-        public string Configuration { get; set; }
-    }
 }
---
+++ Source/LeanBattleship.Web/Controllers/VersionDto.cs
@@ -0,0 +1,9 @@
+namespace LeanBattleship.Web.Controllers
+{
+    public class VersionDto
+    {
+        public string Version { get; set; }
+        public string Build { get; set; }
+        public string Configuration { get; set; }
+    }
+}
